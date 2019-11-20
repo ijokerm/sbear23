8d3e8a35637a5147e376d6808b6b26a24652797a
ijokerm <2013099087@qq.com>
2019-11-20 16:16:25 +0800

    多线程

---
+++ SequencePrint.java
@@ -0,0 +1,13 @@
+package lesson5;
+
+/**
+ * 三个线程A、B、C，分别打印字符串A,B,C
+ * 要求循环打印10次
+ * 打印结果：
+ * ABC
+ * ABC
+ * ...
+ * 以上总共10次
+ */
+public class SequencePrint {
+}
---
+++ StringMemory.java
@@ -0,0 +1,19 @@
+package lesson5;
+
+public class StringMemory {
+    public static void main(String[] args) {
+        String s1="hello";
+        String s2=new String("hello");
+        String s3="hel";
+        String s4="lo";
+        String s5=s3+s4;
+
+        //-128~127
+        Integer i1=128;
+        Integer i2=128;
+
+    }
+
+
+
+}
